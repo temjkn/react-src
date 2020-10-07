79bc016c1625e242c9e56394d57d12687321e397
temjkn <temjkn@gmail.com>
2020-10-07 22:22:36 +0300

    presentationCompanent

--- src/components/Friends/UsersContainer.js
+++ src/components/Friends/UsersContainer.js
@@ -1,7 +1,38 @@
+import * as Axios from 'axios';
 import React from 'react';
 import User from './User';
 import { connect } from "react-redux";
 import { followAC, setTotalUserCountAC,setCurrentPageAC, setUsersAC, unfollowAC } from '../../redux/friendList-reducer';
+
+class UsersContainer extends React.Component{ // презентационная компанента, делает запрос на сервер, полученные данные передает функциональной компаненте
+    constructor(props){
+        super(props);
+
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.usersOnPage}`).then(
+                response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUserCount(response.data.totalCount)
+                }
+            );
+    }
+    onCangedPage = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.usersOnPage}`).then(
+            response => this.props.setUsers(response.data.items)
+        );
+    }
+    render(){
+        return <User
+                totalUsersCount = {this.props.totalUsersCount}
+                usersOnPage = {this.props.usersOnPage}
+                currentPage = {this.props.currentPage}
+                onCangedPage = {this.onCangedPage}
+                users = {this.props.users}
+                follow = {this.props.follow}
+                unfollow = {this.props.unfollow}
+            />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,6 +63,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(User);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
